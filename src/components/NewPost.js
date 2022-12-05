1a7d672f31fec576ce7a417e85cc4a7625cb397d
Wojciech Wojnar <wojciech.wojnar@hcl.com>
2022-12-05 23:26:47 +0100

    added useInput hook

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,25 +1,38 @@
 import { useState } from "react";
+import { addNewPost } from "../api/requests";
 import classes from "../styles/NewPostForm.module.css";
+import useInput from "../hooks/useInput";
 
-const NewPost = () => {
+const NewPost = (props) => {
     let [isExpanded, setIsExpanded] = useState(false);
+    let author = useInput("");
+    let text = useInput("");
 
     const expandHandler = (visible) => {
         setIsExpanded(visible);
     }
 
-    const submitHandler = (e, post) => {
+    const submitHandler = (e) => {
         e.preventDefault();
-        console.log(e);
-        console.log(post);
+
+        addNewPost({
+            author: author.value,
+            content: text.value
+        })
+            .then(returnedPost => props.updatePosts(returnedPost))
+            .catch(err => console.error(err));
     }
 
     return (
         <form onSubmit={submitHandler} onFocus={_ => expandHandler(true)} className={classes.form}>
-            <textarea className={isExpanded ? classes.formPreview : undefined} name="content" required placeholder="Enter new post..." rows={isExpanded ? 5 : 1} />
+            <textarea className={isExpanded ? classes.formPreview : undefined}
+                name="content" required placeholder="Enter new post..." rows={isExpanded ? 5 : 1} 
+                {...text} />
+
             {isExpanded &&
                 <div className={classes.flexRow}>
-                    <input type="text" name="author" required placeholder="Your name..." />
+                    <input type="text" name="author" required placeholder="Your name..."
+                        {...author} />
                     <button className={classes.sendBtn}>Add</button>
                 </div>}
         </form>
